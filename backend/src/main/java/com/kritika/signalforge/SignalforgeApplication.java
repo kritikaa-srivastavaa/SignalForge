9920f53fd77b79d24fb6f36168126d846d2d36fe
Kritika Srivastava <kritikaa.srivastavaa@gmail.com>
2026-09-14 00:14:04 +0530

    Initialize SignalForge Spring Boot backend

---
+++ backend/src/main/java/com/kritika/signalforge/SignalforgeApplication.java
@@ -0,0 +1,13 @@
+package com.kritika.signalforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalforgeApplication.class, args);
+	}
+
+}
